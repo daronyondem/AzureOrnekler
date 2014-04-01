9c0edf18086c5bf0558d5da7246a93f8674c6a74
Daron Yondem <daron@yondem.com>
2014-04-01 03:03:01 +0300

    WebJobs Blob Örneği

---
+++ WebJobsSDK_Preview/WebJobsTestApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.WindowsAzure.Jobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebJobsTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JobHost host = new JobHost();
+            host.RunAndBlock();
+        }
+
+        public static void LowerCase(
+            [BlobInput(@"gelenblobcontainer/{name}")] Stream gelenStream,
+            [BlobOutput(@"gidenblobcontainer/{name}")] Stream gidenStream)
+        {
+            string tumMetin = "";
+
+            using (StreamReader st = new StreamReader(gelenStream))
+            {
+                tumMetin = st.ReadToEnd();
+            }
+
+            using (StreamWriter stw = new StreamWriter(gidenStream))
+            {
+                stw.Write(tumMetin.ToLower());
+            }
+        }
+    }
+}
