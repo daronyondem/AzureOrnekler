47325213c5849f20a9e586a6e7e401affbced11e
Daron Yondem <daron@yondem.com>
2015-04-12 10:11:16 +0300

    Demo Page Added.

---
+++ SDK_V2.5/Table/Samples/Demo.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" Async="true" AutoEventWireup="true" CodeBehind="Demo.aspx.cs" Inherits="Samples.Demo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    </form>
+</body>
+</html>
+
---
+++ SDK_V2.5/Table/Samples/Demo.aspx.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Samples
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
