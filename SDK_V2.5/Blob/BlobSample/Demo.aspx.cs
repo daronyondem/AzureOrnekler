9f101b1ed9760030b69c50562ec2193a2de60416
Daron Yondem <daron@yondem.com>
2015-04-12 09:37:30 +0300

    Container Creation Sample

---
+++ SDK_V2.5/Blob/BlobSample/Demo.aspx.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BlobSample
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            CloudStorageAccount account = CloudStorageAccount.DevelopmentStorageAccount;
+            CloudBlobClient blobClient = account.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("files");
+            container.CreateIfNotExists();
+        }
+
+    }
+}
