a3530f7c30f63c8a584c15083202445c147fb22f
Daron Yondem <daron@yondem.com>
2016-11-17 12:30:49 +0300

    Actor Service Implementation

--- Service_Fabric/WebSvc/Controllers/HomeController.cs
+++ Service_Fabric/WebSvc/Controllers/HomeController.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Mvc;
 using CounterInterfaces;
 using Microsoft.ServiceFabric.Services.Remoting.Client;
 using Microsoft.ServiceFabric.Services.Client;
+using System.Fabric;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using ActorSvc.Interfaces;
 
 namespace WebSvc.Controllers
 {
@@ -27,9 +31,22 @@ namespace WebSvc.Controllers
             return View();
         }
 
-        public IActionResult Contact()
+        public async Task<IActionResult> Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            // Create a randomly distributed actor ID
+            ActorId actorId = ActorId.CreateRandom();
+
+            actorId = new ActorId("NewActor");
+
+            // This only creates a proxy object, it does not activate an actor or invoke any methods yet.
+            IActorSvc myActor = ActorProxy.Create<IActorSvc>(actorId, new Uri("fabric:/Application1/ActorSvcActorService"));
+
+            // This will invoke a method on the actor. If an actor with the given ID does not exist, it will be activated by this method call.
+           
+            int count = await myActor.GetCountAsync();
+            await myActor.SetCountAsync(count + 1);
+
+            ViewData["Message"] = count.ToString();
 
             return View();
         }
