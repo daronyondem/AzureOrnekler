126e01669e4c6e857b5f9453af8fef8f399943fc
Daron Yondem <daron@yondem.com>
2016-11-17 08:58:51 +0300

    Service Fabric Remoting Frond-End Statefull Service Sample

---
+++ Service_Fabric/CounterInterfaces/ICounter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterInterfaces
+{
+    //Step by step guidance is here; https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-add-a-web-frontend 
+    public interface ICounter : IService
+    {
+        Task<long> GetCountAsync();
+    }
+}
---
+++ Service_Fabric/WebSvc/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CounterInterfaces;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using Microsoft.ServiceFabric.Services.Client;
+
+namespace WebSvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            ICounter counter = ServiceProxy.Create<ICounter>(new Uri("fabric:/Application1/StatefulSvc"), new ServicePartitionKey(0));
+
+            long count = await counter.GetCountAsync();
+
+            ViewData["Message"] = count.ToString();
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
