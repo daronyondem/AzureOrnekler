9008cecdf6a642c1c3217cd245db6ea96aa5deb7
Daron Yondem <daron@yondem.com>
2017-04-25 22:40:34 +0300

    Message Delivery Feedback Implemented

--- IoTHub/DeviceOne/SendCloudToDevice/Program.cs
+++ IoTHub/DeviceOne/SendCloudToDevice/Program.cs
@@ -16,6 +16,7 @@ namespace SendCloudToDevice
         {
             Console.WriteLine("Send Cloud-to-Device message\n");
             serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+            ReceiveFeedbackAsync();
 
             Console.WriteLine("Press any key to send a C2D message.");
             Console.ReadLine();
@@ -26,7 +27,26 @@ namespace SendCloudToDevice
         private async static Task SendCloudToDeviceMessageAsync()
         {
             var commandMessage = new Message(Encoding.ASCII.GetBytes("Cloud to device message."));
+            commandMessage.Ack = DeliveryAcknowledgement.Full;
             await serviceClient.SendAsync("myFirstDevice", commandMessage);
+        }
+
+        private async static void ReceiveFeedbackAsync()
+        {
+            var feedbackReceiver = serviceClient.GetFeedbackReceiver();
+
+            Console.WriteLine("\nReceiving c2d feedback from service");
+            while (true)
+            {
+                var feedbackBatch = await feedbackReceiver.ReceiveAsync();
+                if (feedbackBatch == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received feedback: {0}", string.Join(", ", feedbackBatch.Records.Select(f => f.StatusCode)));
+                Console.ResetColor();
+
+                await feedbackReceiver.CompleteAsync(feedbackBatch);
+            }
         }
     }
 }
